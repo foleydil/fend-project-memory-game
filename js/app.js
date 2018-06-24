76a2a7be4bf526e8fae73f540ba48d6cbb623c1a
Dillon Foley <foleydil@gmail.com>
2018-06-24 13:33:25 -0400

    Feat: add click listener to show card when clicked

--- js/app.js
+++ js/app.js
@@ -22,20 +22,19 @@ let deck = [
 
 /*
  * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
+ *   - shuffle the list of cards using the provided "shuffle" method belo
  */
- shuffle(deck);
-
+shuffle(deck);
 let deckUl = document.querySelector('.deck');
 
+//   - loop through each card and create its HTML
 deck.forEach(function(card) {
  let currentCard = document.createElement('li');
- currentCard.classList.add('card', 'open', 'show');
+ currentCard.classList.add('card');
  let cardItem = document.createElement('i');
  cardItem.classList.add('fa', card);
  currentCard.appendChild(cardItem);
+//   - add each card's HTML to the page
  deckUl.appendChild(currentCard);
 });
 
@@ -54,7 +53,6 @@ function shuffle(array) {
     return array;
 }
 
-
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
@@ -65,3 +63,12 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+ function showCard(event) {
+   let clickedCard = event.target;
+   clickedCard.classList.add('open', 'show');
+ }
+
+ deckUl.addEventListener('click', showCard);
+
+let openCards = [];
